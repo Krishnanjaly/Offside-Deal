eea353433dca4555e58147657aa795ffd7ffb4a9
Krishnanjaly <krishnanjalys98@gmail.com>
2025-06-24 21:20:55 +0530

    hi

--- frontend/online-shopping/src/Login/Loginpage.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import Login from './Login/Login.jsx';
-
-const Loginpage = () => {
-  return <Login />;
-};
-
-export default Loginpage;
--- frontend/online-shopping/src/Login/exam.jsx
+++
@@ -1,7 +0,0 @@
-function Exa() {
-    return(
-        <>
-        <h1>hsi</h1></>
-    )
-}
-export default Exa;
